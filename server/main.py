6579ba2eddbf52d4a7716c9056ebc065162ab313
AyushWalekar <walekarayush@gmail.com>
2018-09-29 02:57:37 +0530

    Added template

--- server/main.py
+++ server/main.py
@@ -1,7 +1,7 @@
-from flask import Flask
-from flask import request
+from flask import Flask, render_template
+from flask import request, make_response
 from indictrans import Transliterator
-from .translator import translate
+from translator import translate
 
 import enchant
 from langid.langid import LanguageIdentifier, model
@@ -9,15 +9,25 @@ from langid.langid import LanguageIdentifier, model
 app = Flask(__name__)
 
 
-@app.route("/convert",methods=['POST'])
+@app.route("/")
+def index():
+    return render_template("./test.html")
+
+
+@app.route("/convert",methods=['POST','GET'])
 def convert_():
-    req_data = request.get_json()
-
-    text = req_data['text']
-
+    req_data = request.args
+    # print(req_data['text'])
+    # text = "Ayush"
+    text = req_data['text'].split()[-1]
+    print(text)
     # from here the function will be called which will check and convert the text
     text = translate(trn, text, eng_dict, hin_dict, classifier)
-    return "Response: " + trn.transform(text)
+    print(text)
+    res = make_response(text,200)
+    res.headers['Content-Type'] = 'text/plain'
+    # res.headers['Content-Type'] = 'text/plain'
+    return res
 
 
 if __name__ == "__main__":
@@ -25,4 +35,4 @@ if __name__ == "__main__":
     hin_dict = enchant.Dict('hi_IN')
     trn = Transliterator(source='eng', target='hin')
     classifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
-    app.run()
+    app.run('0.0.0.0')
