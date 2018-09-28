9ee2f5cd357fb0f8c1467e76b42ad581dd7dd24c
milind97 <milind1605@gmail.com>
2018-09-29 05:18:54 +0530

    translation now returns list of suggested words

--- server/translator.py
+++ server/translator.py
@@ -4,26 +4,36 @@ from difflib import get_close_matches
 from langid.langid import LanguageIdentifier, model
 
 
-def translate(trans_obj, word, eng_dict, hin_dict, classifier):
-
-    if eng_dict.check(word):
-        return word
+def translate_in_hindi(trans_obj, word, hin_dict):
+    hindi_word = trans_obj.transform(word)
+    if hin_dict.check(hindi_word):
+        return hindi_word, []
     else:
-        prob = classifier.classify(word)
-        print(prob)
-        if prob[0] == 'en' and prob[1] > 0.5:
-            return word
-        else:
-            print('not english')
-            hindi_word = trans_obj.transform(word)
-            if hin_dict.check(hindi_word):
-                return hindi_word
-            else:
-                matches = get_close_matches(hindi_word, hin_dict.suggest(hindi_word), cutoff=0.7)
-                if matches:
-                    return hindi_word
-                else:
-                    return word
+        matches = get_close_matches(hindi_word, hin_dict.suggest(hindi_word), n=1, cutoff=0.7)
+
+    return hindi_word, matches
+
+
+def translate(trans_obj, word, eng_dict, hin_dict, classifier, flag):
+
+    hindi_word, hindi_match = translate_in_hindi(trans_obj, word, hin_dict)
+    print('hin_word, hindi_matches', hindi_word, hindi_match)
+    suggested_words = [[hindi_word, 1]]
+    print('s', suggested_words)
+    prob = classifier.classify(word)
+    if not flag:
+        if eng_dict.check(word) or (prob[0] == 'en' and prob[1] > 0.5):
+            print('here')
+            suggested_words = [[word, 0]] + suggested_words
+            print('s', suggested_words)
+
+    else:
+        suggested_words.append([word, 0])
+    print(suggested_words)
+    if hindi_match:
+        suggested_words.append([hindi_match[0], 1])
+
+    return suggested_words
 
 
 if __name__ == '__main__':
@@ -32,10 +42,8 @@ if __name__ == '__main__':
     trn = Transliterator(source='eng', target='hin')
     classifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
     sentence = list(input().split())
-    ts = ''
     for word in sentence:
-        ts = ' '.join([ts, translate(trn, word, eng_dict, hin_dict, classifier)])
-    print('translated: ', ts)
+        print(translate(trn, word, eng_dict, hin_dict, classifier, 0))
 
 
 
